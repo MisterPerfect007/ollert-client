0099034fed7b4ba6aba46056646b7082294f70f1
Parfait Tapsoba <leneodepart@gmail.com>
2021-10-31 18:52:56 +0000

    Foders structure and Header component building

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import "./Header.css";
+import logo from "../../Assets/Logo.svg";
+import { IconContext } from "react-icons";
+import { CgLayoutGridSmall } from "react-icons/cg";
+import { RiArrowDownSFill } from "react-icons/ri";
+
+
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img className="header--logo" src={logo} alt="logo" />
+                <div className="header--boardInfo">
+                    <h1 className="header--boardInfo_Title">DevChallenge Board</h1>
+                    <div className="header--boardInfo_btn">
+                        <IconContext.Provider value={{color: "var(--gray-3)",size: "1.5em"}}>
+                            <div className="d-f">
+                                <CgLayoutGridSmall />
+                            </div>
+                        </IconContext.Provider>
+                        <h3>All Board</h3>
+                    </div>
+                </div>
+            </div>
+            {/* The Right side */}
+            <div className="header__right">
+                <div className="header--search">
+                    <input 
+                        className="header--search_input" 
+                        type="text"
+                        placeholder="Keywords ..."
+                    />
+                    <button className="header--search_btn">Search</button>
+                </div>
+                <div className="header--userInfo">
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                    <h3 className="header--userInfo_name">first name last name</h3>
+                    <RiArrowDownSFill />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/Routes/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../Components/Header/Header';
+
+function Home() {
+    return (
+        <div className="home">
+            <Header />
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export default Home
